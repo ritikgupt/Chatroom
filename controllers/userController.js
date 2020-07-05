fc5ff19591220a5a601e4e6cdd68b62d62393c4c
ritikgupt <ritikgupta89369@gmail.com>
2020-07-05 17:27:59 +0530

    authentication updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,45 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-const sha256 = require("js-sha256");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const jwt = require("jwt-then");
 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const emailRegex = /@gmail.com|@yahoo.com|@hotmail.com|@live.com/;
-
-  if (!emailRegex.test(email)) throw "Email is not supported from your domain.";
-  if (password.length < 6) throw "Password must be atleast 6 characters long.";
-
-  const userExists = await User.findOne({
-    email,
-  });
-
-  if (userExists) throw "User with same email already exits.";
-
-  const user = new User({
-    name,
-    email,
-    password: sha256(password + process.env.SALT),
-  });
-
-  await user.save();
-
-  res.json({
-    message: "User [" + name + "] registered successfully!",
-  });
+ try{
+    await bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      User.create({
+         name:req.body.name,
+         email:req.body.email,
+         password:hash
+        })
+     })
+    res.json({
+        message: "User  registered successfully!",
+      });
+ }catch(e){
+     res.json({message:e})
+ } 
 };
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({
     email,
-    password: sha256(password + process.env.SALT),
   });
-
-  if (!user) throw "Email and Password did not match.";
-
-  const token = await jwt.sign({ id: user.id }, process.env.SECRET);
-
-  res.json({
-    message: "User logged in successfully!",
-    token,
-  });
-};
+  console.log(user)
+  if (!user) throw "Email is incorrect";
+  await bcrypt.compare(password,user.password,async (err,result)=>{
+      console.log('hi')
+      console.log(result)
+      if(result==true)
+        {
+            const token = await jwt.sign({ id: user.id }, process.env.SECRET);
+            res.json({
+              message: "User logged in successfully!",
+              token,
+            });
+          }
+        else{
+           res.json({message:'incorrect password entered'})
+        }
+          })}
