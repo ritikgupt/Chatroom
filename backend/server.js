932a8721f07b0038fdf12b843b8f22367fc6ba26
ritikgupt <ritikgupta89369@gmail.com>
2020-07-06 14:36:00 +0530

    chatroom completed

--- backend/server.js
+++ backend/server.js
@@ -25,28 +25,52 @@ const server = app.listen(8000, () => {
   console.log("Server listening on port 8000");
 });
 
-const io = require('socket.io')(server);
-
+const io = require("socket.io")(server);
 const jwt = require("jwt-then");
 
-io.use(async (socket,next)=>{
+const Message = mongoose.model("Message");
+const User = mongoose.model("User");
 
-try {
-  const token = socket.handshake.query.token
-  const payload = await jwt.verify(token, process.env.SECRET);
-  socket.userId=payload.id
-  next();
-} catch (err) {
-  res.status(401).json({
-    message: "Forbidden ",
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.query.token;
+    const payload = await jwt.verify(token, process.env.SECRET);
+    socket.userId = payload.id;
+    next();
+  } catch (err) {}
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected: " + socket.userId);
   });
-}
-})
 
-io.on('connection',(socket)=>{
-  console.log('connected'+ socket.userId)
+  socket.on("joinRoom", ({ chatroomId }) => {
+    socket.join(chatroomId);
+    console.log("A user joined chatroom: " + chatroomId);
+  });
 
-  socket.on('disconnect',()=>{
-    console.log("Disconneted:" + socket.userId)
-  })
-})
+  socket.on("leaveRoom", ({ chatroomId }) => {
+    socket.leave(chatroomId);
+    console.log("A user left chatroom: " + chatroomId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Message({
+        chatroom: chatroomId,
+        user: socket.userId,
+        message,
+      });
+      io.to(chatroomId).emit("newMessage", {
+        message,
+        name: user.name,
+        userId: socket.userId,
+      });
+      await newMessage.save();
+    }
+  });
+});
